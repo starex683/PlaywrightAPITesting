de8e87d9bba6bc3e4305384e576dc231cbc5123b
Akhila <starex683@gmail.com>
2026-07-03 10:40:32 +0100

    Update test data for API requests

--- tests/patch_api_request.spec.js
+++ tests/patch_api_request.spec.js
@@ -16,8 +16,8 @@ test("Query parameter in playwright api testing", async ({ request }) => {
   // create GET api request using playwright
   const getAPIResponse = await request.get("/booking/", {
     params: {
-      firstname: "testers talk playwright",
-      lastname: "testers talk api testing",
+      firstname: "akhila playwright",
+      lastname: "akhila api testing",
     },
   });
 
@@ -44,8 +44,8 @@ test("Query parameter in playwright api testing", async ({ request }) => {
       "Cookie": `token=${tokenNo}`
     },
     data: {
-      firstname: "testers talk postman",
-      lastname: "testers talk rest assured"
+      firstname: "akhila postman",
+      lastname: "akhila rest assured"
     },
   });
 
